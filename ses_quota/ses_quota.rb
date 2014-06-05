281e1d08af77a9f1abdc6951fad268cfbb592021
Anthony Powles <anthony@anyperk.com>
2014-06-05 13:44:32 -0700

    added custom SES quota plugin

---
+++ ses_quota/ses_quota.rb
@@ -0,0 +1,24 @@
+class SesQuota < Scout::Plugin
+  needs :json
+
+  OPTIONS=<<-EOS
+    region:
+      name: SES Region
+      notes: Specify the AWS region
+      default: us-east-1
+  EOS
+
+  def build_report
+    report(quota)
+  end
+
+  private
+
+  def quota
+    # expects AWS CLI to be available and configured
+    JSON.parse(`aws ses get-send-quota --region=#{option(:region)}`)
+  rescue
+    {}
+  end
+
+end
